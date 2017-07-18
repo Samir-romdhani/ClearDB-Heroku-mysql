04a48fa1e6ec5198c5db9850cabdce975a9d18f5
Samir-Romdhani <romdhanisamir94@gmail.com>
2017-07-18 18:00:52 +0100

    MARIA Database with Heroku Server

--- web.js
+++ web.js
@@ -3,17 +3,17 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-
+/*
 app.get('/', function(request, response) {
 response.send('Hello World!!!!test0 --> test1') ;
 }) ;
+*/
 
-/*
 var connection = mysql.createConnection({
-    host: 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'vbebknxc303ntx0f',
-    password: 'nnzetk5uihgms2l8',
-    database: 'vti0cwdbw6zs5nka'
+    host: 'olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'q2yseffhsh99cnbt',
+    password: '	fdc1grofl58s3rqc',
+    database: 'ec3n49dqttudnvr0'
 });
 
 connection.connect();
@@ -30,7 +30,6 @@ app.get('/', function(request, response) {
         
 });
 
-*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
