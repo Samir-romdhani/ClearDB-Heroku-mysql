a4204f6f2be94dd5451d0d0a437cce2fe929e1d3
Samir-Romdhani <romdhanisamir94@gmail.com>
2017-07-18 18:46:23 +0100

    Cleardb with heroku server github example web.js

--- web.js
+++ web.js
@@ -1,36 +1,55 @@
+
 var express = require("express");
 var mysql = require('mysql');
 var app = express();
 app.use(express.logger());
 
-/*
-app.get('/', function(request, response) {
-response.send('Hello World!!!!test0 --> test1--> test2') ;
-}) ;
-
-*/
-var connection = mysql.createConnection({
+var db_config = {
     host: 'us-cdbr-iron-east-03.cleardb.net',
     user: 'bbd7ce5b72d0bc',
     password: 'd782912a',
     database: 'heroku_227dbd8f597ec9f'
-});
+};
 
-connection.connect();
+var connection;
+
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 app.get('/', function(request, response) {
-	    connection.query('SELECT * from t_users', function(err, rows, fields) {
+    connection.query('SELECT * from t_users', function(err, rows, fields) {
         if (err) {
             console.log('error: ', err);
             throw err;
         }
-      //  response.send(['Hello World!!!! HOLA MUNDO!!!! Samir', rows]);
+        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
     });
-        
-        
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
+
+
